9ad159155a24e2948137670647508876a9501684
Gregori Simei <gregorisimei@gmail.com>
2020-02-15 02:57:31 -0200

    Calendar UX defined

--- main.py
+++ main.py
@@ -13,24 +13,28 @@ class Menu(Screen):
 	def calendario(self):
 		arq   = open('./arq/calendario.json','r')
 		try:
-			print ("entrou")
+			print ("entrou - abriu arquivo")
 			print (arq)
-			datas = json.loads(arq)
+			datas = json.load(arq)
 			print (datas)
 			print("não deu erro ")
 			for data in datas:
-				print("lendo")
-				print (data)
+				for label in data:
+					if label == "horario":
+						self.ids.calendario_Scroll.add_widget(Label(text=data[label], size_hint_y=None, height=200, color=(0,0,0,1)))
 			print ("leu")
 		except:
 			arq.close()
-			print("deu erro")
+			print("deu erro - fechou arquivo")
 		else:
 			arq.close()
-			print("terminou com exito")
+			print("terminou com exito - fechou arquivo")
 		'''calendario = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
 		for texto in calendario:
 			self.ids.calendario_Scroll.add_widget(Label(text=texto, size_hint_y=None, height=200, color=(0,0,0,1)))'''
+
+	def dinamicContentCalendar (self):
+		pass
 
 class Culturajovem(App):
     def build(self):
