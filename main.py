46f464586c40bb91271b0f450dbdf24b174ae783
Gregori Simei <gregorisimei@gmail.com>
2020-02-17 22:35:27 -0300

    Building adding data dynamically with graphic interface

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app 				import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.label 		import Label
 from kivy.uix.boxlayout 	import BoxLayout
+from kivy.graphics import *
+from kivy.graphics.vertex_instructions import *
 import json
 
 
@@ -10,31 +12,81 @@ class TelaManager(ScreenManager):
 
 class Menu(Screen):
 
+
 	def calendario(self):
 		arq   = open('./arq/calendario.json','r')
 		try:
 			print ("entrou - abriu arquivo")
-			print (arq)
 			datas = json.load(arq)
-			print (datas)
-			print("não deu erro ")
-			for data in datas:
-				for label in data:
-					if label == "horario":
-						self.ids.calendario_Scroll.add_widget(Label(text=data[label], size_hint_y=None, height=200, color=(0,0,0,1)))
-			print ("leu")
+			print("não deu erro ao ler arquivo")
+			print("Enviando arquivo")
+			for data in datas :
+				self.dinamicContentCalendar(data)	
+			print ("leu o arquivo")
 		except:
 			arq.close()
 			print("deu erro - fechou arquivo")
 		else:
 			arq.close()
 			print("terminou com exito - fechou arquivo")
-		'''calendario = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-		for texto in calendario:
-			self.ids.calendario_Scroll.add_widget(Label(text=texto, size_hint_y=None, height=200, color=(0,0,0,1)))'''
 
-	def dinamicContentCalendar (self):
-		pass
+	def dinamicContentCalendar (self, evento):
+
+		print("começou a criar parte gráfica")
+
+		# Container base para ser montado a parte gráfica de cada bloco do calendario
+		boxContainer = BoxLayout()
+		boxContainer.size_hint_y = None
+		boxContainer.width = 200
+		boxContainer.padding = '10sp'
+		print("terminou de criar o boxContainer")
+
+		# Apenas enfeite que ficara a direita, dentro do boxContainer
+		leftLineInsideBoxContainer = Label()
+		leftLineInsideBoxContainer.size_hint_x = 0.02
+		with leftLineInsideBoxContainer.canvas:
+			print("Começou a criar o canvas")
+			Color(rgba = (0.65, 0.65, 0.65, 1))
+			print("estabeleceu uma cor")
+			Rectangle(pos = (leftLineInsideBoxContainer.x+(leftLineInsideBoxContainer.width*1.5)/2, leftLineInsideBoxContainer.y), 
+					  size = (leftLineInsideBoxContainer.width/10, leftLineInsideBoxContainer.height))
+			print("criou o retangulo")
+			Color(0.8, 0.8, 0.8,1)
+			Ellipse(pos = (leftLineInsideBoxContainer.x, leftLineInsideBoxContainer.y+(leftLineInsideBoxContainer.height/2)-(leftLineInsideBoxContainer.width/2)), 
+					size = (leftLineInsideBoxContainer.width*1.5, leftLineInsideBoxContainer.width*1.5))
+		print("Terminou de criar leftLineInsideBoxContainer")
+
+		# No hourBox estará contida a hora do envento
+		hourBox = Label()
+		hourBox.color = (0, 0, 0, 0.5)
+		hourBox.size_hint_x = 0.2
+		print("terminou de criar hourBox")
+
+		# No titleBox estará contido o titulo do evento
+		titleBox = Label()
+		titleBox.canvas.before.add(Color(rgba = (0, 0.5, 0.9, 1)))
+		titleBox.canvas.before.add(RoundedRectangle(size = titleBox.size, 
+													pos = titleBox.pos, 
+													radius = [(10.0, 10.0), (10.0, 10.0), (10.0, 10.0), (10.0, 10.0)]))
+		print("Terminou de criar titleBox")
+
+		for data in evento:
+			print("entrou para adicionar text para as labels")
+			print(data)
+			if data == "horario":
+				hourBox.text = evento[data]
+			if data == "titulo":
+				titleBox.text = evento[data]
+			print("Terminou de adcionar o text")
+
+		print("Começando a adicionar widgets no boxContainer")
+		'''boxContainer.add_widget(leftLineInsideBoxContainer)
+		boxContainer.add_widget(hourBox)'''
+		boxContainer.add_widget(titleBox)
+		print("Terminou de adicionar widgets no boxContainer")
+
+		self.ids.calendario_Scroll.add_widget(boxContainer)
+
 
 class Culturajovem(App):
     def build(self):
