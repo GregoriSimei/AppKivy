7a05f01add527d14b703a6084545596e8649a12b
Gregori Simei <gregorisimei@gmail.com>
2020-02-19 01:16:21 -0300

    Testing internet connection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from kivy.app 							import App
 from kivy.uix.screenmanager 			import Screen, ScreenManager
 from kivy.uix.label 					import Label
@@ -5,7 +7,6 @@ from kivy.uix.boxlayout 				import BoxLayout
 from kivy.graphics 						import *
 from kivy.graphics.vertex_instructions 	import *
 from kivy.lang 							import Builder
-import json
 
 
 class TelaManager(ScreenManager):
@@ -15,10 +16,12 @@ class Menu(Screen):
 
 
 	def calendario(self):
-		arq   = open('./arq/calendario.json','r')
+		arq   = open('./arq/calendario.json','w')
 		try:
-			datas = json.load(arq)
-			for data in datas :
+			getApi = requests.get("https://boldeagles.000webhostapp.com/calendario.php")
+			arq.write(json.dumps(getApi.text))
+			jsonGet = json.loads(getApi.text)
+			for data in jsonGet :
 				self.dinamicContentCalendar(data)
 		except:
 			arq.close()
@@ -34,26 +37,28 @@ class Menu(Screen):
 		# Container base para ser montado a parte gráfica de cada bloco do calendario
 		boxContainer = BoxLayout()
 		boxContainer.size_hint_y = None
-		boxContainer.width = 200
-		boxContainer.padding = '10sp'
+		boxContainer.width = 170
+		boxContainer.padding = '5sp'
 
 		# Apenas enfeite que ficara a direita, dentro do boxContainer
 		leftLineInsideBoxContainer = Label()
 		leftLineInsideBoxContainer.size_hint_x = 0.02
 		with leftLineInsideBoxContainer.canvas:
 			Color(rgba = (0.65, 0.65, 0.65, 1))
-			leftLineInsideBoxContainer.rect = Rectangle(pos = (leftLineInsideBoxContainer.x+(leftLineInsideBoxContainer.width*1.5)/2, leftLineInsideBoxContainer.y), 
-					  									size = (leftLineInsideBoxContainer.width/10, leftLineInsideBoxContainer.height))
+			leftLineInsideBoxContainer.rect = Rectangle(pos  = (leftLineInsideBoxContainer.x+(leftLineInsideBoxContainer.width*1.5)/2, leftLineInsideBoxContainer.y), 
+					  									size = (leftLineInsideBoxContainer.width/500, leftLineInsideBoxContainer.height))
 			Color(0.8, 0.8, 0.8,1)
 			'''
 			Ellipse(pos = (leftLineInsideBoxContainer.x, leftLineInsideBoxContainer.y+(leftLineInsideBoxContainer.height/2)-(leftLineInsideBoxContainer.width/2)), 
-					size = (leftLineInsideBoxContainer.width*1.5, leftLineInsideBoxContainer.width*1.5)) '''
+					size = (leftLineInsideBoxContainer.width*1.5, leftLineInsideBoxContainer.width*1.5))
+			'''
 
 		# Altera o tamanho e a posição do widget leftLineInsideBoxContainer
 		leftLineInsideBoxContainer.bind(pos=update_rect, size=update_rect)
 
 		# No hourBox estará contida a hora do envento
 		hourBox = Label()
+		hourBox.font_size = '10sp'
 		hourBox.color = (0, 0, 0, 0.5)
 		hourBox.size_hint_x = 0.2
 
@@ -70,9 +75,9 @@ class Menu(Screen):
 
 		# Adiciona os textos nos widgets
 		for data in evento:
-			if data == "horario":
-				hourBox.text = evento[data]
-			if data == "titulo":
+			if data == "hour":
+				hourBox.text = evento[data][0:5]
+			if data == "title":
 				titleBox.text = evento[data]
 
 		boxContainer.add_widget(leftLineInsideBoxContainer)
